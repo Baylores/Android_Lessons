e9d050df5ab1152b262cf9c118737a4c14d3d9c7
SmarttGuy <paul.sav21@gmail.com>
2016-09-23 20:51:06 +0400

    Init

---
+++ Androidlessons/p0211twoactivity/src/main/java/ru/startandroid/p0211twoactivity/ActivityTwo.java
@@ -0,0 +1,17 @@
+package ru.startandroid.p0211twoactivity;
+
+/**
+ * Created by overl on 19.09.2016.
+ */
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ActivityTwo extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.two);
+    }
+}
